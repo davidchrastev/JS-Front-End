2ee7db43fa0cd3fb53da642a2f3db82381515f31
Давид Кръстев <106105745+davidchrastev@users.noreply.github.com>
2023-02-21 19:43:55 +0200

    Practicing JS syntax

--- JS Syntax Fundamentals/02-student-info.js
+++ JS Syntax Fundamentals/02-student-info.js
@@ -1,6 +1,8 @@
 function solve(name,age,grade) {
-    
-    console.log('Name: ${name}');
+    console.log('Name: ' + name 
+    + ', Age: ' + age 
+    + ', Grade: ' + Number.parseFloat(grade).toFixed(2));
 }
 
-solve('Name', 13, 31)
+
+solve('Name', 13, 31.000)
