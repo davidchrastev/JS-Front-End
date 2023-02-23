fede25bf09e439481f62f91cc5efd5fba8124440
Давид Кръстев <106105745+davidchrastev@users.noreply.github.com>
2023-02-23 21:25:08 +0200

    Solution without regex

--- JS Syntax Fundamentals – Exercises/18-Modern-Times-Of-Hashtag.js
+++ JS Syntax Fundamentals – Exercises/18-Modern-Times-Of-Hashtag.js
@@ -1,16 +1,36 @@
 function findSpecialWords(input) {
-  const regex = /#(\w+)\b/g;
-  const matches = input.match(regex);
-  const specialWords = [];
+  // const regex = /#(\w+)\b/g;
+  // const matches = input.match(regex);
+  // const specialWords = [];
 
-  if (matches !== null) {
-    matches.forEach((match) => {
-      const word = match.slice(1);
-      if (/^[a-zA-Z]+$/.test(word)) {
-        specialWords.push(word);
-      }
-    });
+  // if (matches !== null) {
+  //   matches.forEach((match) => {
+  //     const word = match.slice(1);
+  //     if (/^[a-zA-Z]+$/.test(word)) {
+  //       specialWords.push(word);
+  //     }
+  //   });
+  // }
+
+  // return specialWords.join("\n");
+
+  let text = input;
+
+  let words = text.split(" ");
+  let result = [];
+
+  for (const word of words) {
+    if (word.length > 1 && word.startsWith("#") && checkIfWordIsValid(word)) {
+      result.push(word.slice(1));
+    }
   }
 
-  return specialWords.join("\n");
+  console.log(result.join('\n'));
+
+  function checkIfWordIsValid(currentWord) {
+      return [...currentWord.toLowerCase()]
+      .slice(1)
+      .map((symbol) => symbol.charCodeAt(0))
+      .every((charCode) => charCode >= 97 && charCode <= 122);
+  }
 }
