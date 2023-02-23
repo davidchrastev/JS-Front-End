ad914d4a2b867010bf91b28bdc7847a2c8343171
Давид Кръстев <106105745+davidchrastev@users.noreply.github.com>
2023-02-23 20:38:25 +0200

    Different strategy to solve the problem

--- JS Syntax Fundamentals – Exercises/16-Sorting-Number.js
+++ JS Syntax Fundamentals – Exercises/16-Sorting-Number.js
@@ -1,16 +1,36 @@
 function sortArray(arr) {
-  arr.sort((a, b) => a - b); // sort the array in ascending order
+
+  // arr.sort((a, b) => a - b); // sort the array in ascending order
+  // let result = [];
+  // let i = 0;
+  // let j = arr.length - 1;
+  // while (i <= j) {
+  //   if (i === j) {
+  //     result.push(arr[i]); // push the last element if there's only one left
+  //   } else {
+  //     result.push(arr[i], arr[j]); // push the smallest and largest elements
+  //   }
+  //   i++;
+  //   j--;
+  // }
+  // return result;
+
+  let sorted = [...arr].sort((a, b) => a - b);
+  let step = 0;
   let result = [];
-  let i = 0;
-  let j = arr.length - 1;
-  while (i <= j) {
-    if (i === j) {
-      result.push(arr[i]); // push the last element if there's only one left
+  
+  while (sorted.length > 0) {
+    if (step % 2 == 0) {
+      let firstEl = result.shift();
+      result.push(firstEl);
     } else {
-      result.push(arr[i], arr[j]); // push the smallest and largest elements
+      let lastEl = result.pop();
+      result.push(lastEl);
     }
-    i++;
-    j--;
+
+    step++;
   }
+
   return result;
+
 }
