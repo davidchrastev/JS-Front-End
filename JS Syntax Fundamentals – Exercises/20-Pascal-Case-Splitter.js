f9b36e4ac8211e383daed4f813061cbec492b9ea
Давид Кръстев <106105745+davidchrastev@users.noreply.github.com>
2023-02-23 22:04:53 +0200

    Different way of solving

--- JS Syntax Fundamentals – Exercises/20-Pascal-Case-Splitter.js
+++ JS Syntax Fundamentals – Exercises/20-Pascal-Case-Splitter.js
@@ -1,4 +1,19 @@
 function splitPascalCase(input) {
+  // let splittedText = text.split("");
+  // let result = [];
+  // while (splittedText.length > 0) {
+  //   let currentWord = "";
+  //   currentWord += splittedText.shift();
+  //   while (
+  //     splittedText.length > 0 &&
+  //     splittedText[0] === splittedText[0].toLowerCase()
+  //   ) {
+  //     currentWord += splittedText.shift();
+  //   }
+  //   result.push(currentWord);
+  // }
+  // console.log(result.join(", "));
+
   const regex = /[A-Z][a-z]*/g;
   const matches = input.match(regex);
   return matches.join(", ");
