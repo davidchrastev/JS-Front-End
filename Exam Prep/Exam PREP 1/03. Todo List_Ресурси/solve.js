0dfca54a5a21b7d34da1086ea6f14d66748f0f19
Давид Кръстев <106105745+davidchrastev@users.noreply.github.com>
2023-04-06 16:01:35 +0300

    Nice structured solution in JS

--- Exam Prep/Exam PREP 1/03. Todo List_Ресурси/solve.js
+++ Exam Prep/Exam PREP 1/03. Todo List_Ресурси/solve.js
@@ -1,106 +1,145 @@
-// TODO
 function attachEvents() {
-  const BASE_URL = 'http://localhost:3030/jsonstore/tasks/';
-  const titleInput = document.getElementById('title')
-  const loadBtn = document.getElementById('load-button')
-  const addBtn = document.getElementById('add-button')
-  const todoListContainer = document.getElementById('todo-list')
+    const BASE_URL = 'http://localhost:3030/jsonstore/tasks/';
 
-  loadBtn.addEventListener("click", loadTasksHandler)
-  addBtn.addEventListener("click", addTaskHandler)
-  function loadTasksHandler(event) {
-      if (event){
-          event.preventDefault()
-      }
+    const input = document.getElementById('title');
+    const addBTN = document.getElementById('add-button');
+    const loadBTN = document.getElementById('load-button');
 
-      todoListContainer.innerHTML = ''
-      fetch(BASE_URL)
-          .then((data) => data.json())
-          .then((tasksRes) => {
-              const tasks = Object.values(tasksRes)
-              console.log(tasks);
-              for (const {_id, name} of tasks) {
-                  const li = document.createElement('li')
-                  const span = document.createElement('span')
-                  const removeBtn = document.createElement('button')
-                  const editBtn = document.createElement('button')
+    const container = document.getElementById('todo-list');
 
-                  li.id = _id
-                  span.textContent = name
-                  removeBtn.textContent = 'Remove'
-                  editBtn.textContent = 'Edit'
+    loadBTN.addEventListener('click', loadTasks);
+    addBTN.addEventListener('click', addTask);
+    let currentTasks = [];
+    function loadTasks(event) {
+        if (event) {
+            event.preventDefault();
+        }
+        container.innerHTML = '';
 
-                  editBtn.addEventListener("click", loadEditFormHandler)
-                  removeBtn.addEventListener("click", removeTaskHandler)
+        fetch(BASE_URL)
+            .then((response) => response.json())
+            .then((tasks) => {
+                currentTasks = Object.values(tasks);
+                for ( const {name, _id} of currentTasks) {
+                    const li = createElement('li', container, null, null, _id);
+                    createElement('span', li, `${name}`);
+                    const removeBtn = createElement('button', li, 'Remove');
+                    const editBtn = createElement('button', li, 'Edit');
 
-                  li.append(span, removeBtn, editBtn)
-                  todoListContainer.appendChild(li)
-              }
-          })
-          .catch((err) => {
-              console.error(err);
-          })
-  }
+                    removeBtn.addEventListener('click', remove);
+                    editBtn.addEventListener('click', edit);
 
-  function loadEditFormHandler(event){
-      const liParent = event.currentTarget.parentNode
-      const [span, _removeBtn, editBtn] = Array.from(liParent.children)
-      const editInput = document.createElement('input')
-      editInput.value = span.textContent
-      liParent.prepend(editInput)
-      const submitBtn = document.createElement('button')
-      submitBtn.textContent = 'Submit'
-      submitBtn.addEventListener("click", submitTaskHandler)
-      liParent.appendChild(submitBtn)
-      span.remove()
-      editBtn.remove()
-  }
+                }
+            })
+            .catch(err => {
+                console.error(err);
+            });
+    }
+    function remove(event) {
+        let parent = event.currentTarget.parentNode;
+        let id = parent.id;
 
-  function submitTaskHandler(event) {
-      const liParent = event.currentTarget.parentNode
-      const id = event.currentTarget.parentNode.id
-      const [input] = Array.from(liParent.children)
-      const httpHeaders = {
-          method: 'PATCH',
-          body: JSON.stringify({name: input.value})
-      }
+        const httpHeaders = {
+            method: 'DELETE'
+        }
 
-      fetch(`${BASE_URL}${id}`, httpHeaders)
-          .then(() => loadTasksHandler())
-          .catch((err) =>{
-              console.error(err)
-          })
-  }
+        fetch(`${BASE_URL}${id}`, httpHeaders)
+            .then(() => {
+                loadTasks();
+            }).catch(err => {
+                console.error(err);
+        })
+    }
+    function addTask(event) {
+        if (event) {
+            event.preventDefault();
+        }
 
-  function addTaskHandler(event) {
-      event.preventDefault()
-      const name = titleInput.value
-      const httpHeaders = {
-          method: 'POST',
-          body: JSON.stringify({name})
-      }
-      fetch(BASE_URL, httpHeaders)
-          .then(() => {
-              loadTasksHandler()
-              titleInput.value = ''
-          })
-          .catch((err) => {
-              console.error(err)
-          })
+        const payload = JSON.stringify({
+           name: input.value,
+        });
 
-  }
+        const httpHeader = {
+            method: 'POST',
+            body: payload,
+        }
 
-  function removeTaskHandler(event) {
-      const id = event.currentTarget.parentNode.id
-      const httpHeaders = {
-          method: "DELETE",
-      }
-      fetch(`${BASE_URL}${id}`, httpHeaders)
-          .then(() => loadTasksHandler())
-          .catch((err) => {
-              console.error(err)
-          })
-  }
+        fetch(BASE_URL, httpHeader)
+            .then( () => {
+                loadTasks();
+            })
+            .catch(err => {
+                console.error(err);
+            })
+    }
+    function edit(event) {
+        const liParent = event.currentTarget.parentNode;
+        const [span, _removeBtn, editBtn] = Array.from(liParent.children);
+        const editInput = document.createElement('input');
+        editInput.value = span.textContent;
+        liParent.prepend(editInput);
+
+        const submitBtn = document.createElement('button');
+        submitBtn.textContent = 'Submit';
+        submitBtn.addEventListener("click", submit);
+        liParent.appendChild(submitBtn)
+        span.remove();
+        editBtn.remove();
+
+    }
+    function submit(event) {
+        const liParent = event.currentTarget.parentNode
+        const id = event.currentTarget.parentNode.id
+        const [input] = Array.from(liParent.children)
+        const httpHeaders = {
+            method: 'PATCH',
+            body: JSON.stringify({name: input.value})
+        }
+
+        fetch(`${BASE_URL}${id}`, httpHeaders)
+            .then(() => loadTasks())
+            .catch((err) =>{
+                console.error(err)
+            })
+    }
+
+    function createElement(type, parentNode, content, classes, id, attributes, useInnerHtml) {
+        const htmlElement = document.createElement(type);
+
+        if (content && useInnerHtml) {
+            htmlElement.innerHTML = content;
+        } else {
+            if (content && type !== 'input') {
+                htmlElement.textContent = content;
+            }
+
+            if (content && type === 'input') {
+                htmlElement.value = content;
+            }
+        }
+
+        if (classes && classes.length > 0) {
+            htmlElement.classList.add(...classes);
+        }
+
+        if (id) {
+            htmlElement.id = id;
+        }
+
+        // { src: 'link', href: 'http' }
+        if (attributes) {
+            for (const key in attributes) {
+                htmlElement.setAttribute(key, attributes[key])
+            }
+        }
+
+        if (parentNode) {
+            parentNode.appendChild(htmlElement);
+        }
+
+        return htmlElement;
+    }
+
 }
 
 
